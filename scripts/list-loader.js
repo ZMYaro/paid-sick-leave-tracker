5b2e014214cc250e5a33f4d93d2b109ff60f5721
Zachary Yaro <zmyaro@zmyaro.com>
2020-03-30 19:02:26 -0400

    Add status messages to restaurant list and refactor restaurant list render function and styles for buttons

--- scripts/list-loader.js
+++ scripts/list-loader.js
@@ -1,18 +1,25 @@
 'use strict';
 
-const DATA_STATUS_GOOD = 'full',
-	DATA_STATUS_WARN = 'covidOnly',
-	DATA_STATUS_BAD = 'none',
-	DATA_STATUS_CLOSED = 'closed',
-	CSS_GOOD = 'status-good',
-	CSS_WARN = 'status-warn',
-	CSS_BAD = 'status-bad',
-	CSS_CLOSED = 'status-closed',
-	ICON_GOOD = '<img src="images/good.svg" alt="Status: good." />',
-	ICON_WARN = '<img src="images/warn.svg" alt="Status: warning." />',
-	ICON_BAD = '<img src="images/bad.svg" alt="Status: bad." />',
-	ICON_CLOSED = '<img src="images/no_entry.svg" alt="Status: closed." />',
-	ICON_STATEMENT_LINK = '<img src="images/open_in_new.svg" alt="Official statement." title="Official statement" />';
+const ICON_STATEMENT_LINK = '<img src="images/open_in_new.svg" alt="Official statement." title="Official statement" />',
+	STATUS_CSS = {
+		good: 'status-good',
+		warn: 'status-warn',
+		bad: 'status-bad',
+		closed: 'status-closed'
+	},
+	DEFAULT_STATUS_MESSAGES = {
+		good: 'Paid sick leave guaranteed for all workers',
+		warn: 'Paid sick leave only for COVID-19, or varies by location',
+		bad: 'No guaranteed paid sick leave',
+		closed: 'Closed until further notice',
+		undefined: 'No further information available at this time'
+	},
+	STATUS_ICONS = {
+		good: '<img src="images/good.svg" alt="Status: good." />',
+		warn: '<img src="images/warn.svg" alt="Status: warning." />',
+		bad: '<img src="images/bad.svg" alt="Status: bad." />',
+		closed: '<img src="images/no_entry.svg" alt="Status: closed." />'
+	};
 
 var restaurants,
 	restaurantList;
@@ -35,40 +42,49 @@ async function loadRestaurants() {
 }
 
 function renderRestaurants(restaurantData) {
+	function makeStatusButton(status, statusMessage) {
+		statusMessage =
+			statusMessage ||
+			DEFAULT_STATUS_MESSAGES[status] ||
+			DEFAULT_STATUS_MESSAGES.undefined;
+		
+		var button = document.createElement('button');
+		button.title = statusMessage;
+		button.setAttribute('aria-label', statusMessage);
+		button.onclick = function () { alert(this.title); };
+		button.innerHTML = STATUS_ICONS[status] || '';
+		return button;
+	}
+	
 	restaurants = restaurantData;
 	
 	restaurants.forEach((restaurant) => {
 		// Create the restaurant's table row.
 		var row = document.createElement('tr'),
-			rowHTML = '';
+			statusCell = document.createElement('td'),
+			nameCell = document.createElement('td'),
+			locationCell = document.createElement('td'),
+			linkCell = document.createElement('td');
 		
 		// Check status.
-		rowHTML += '<td>';
-		if (restaurant.status === DATA_STATUS_GOOD) {
-			row.classList.add(CSS_GOOD);
-			rowHTML += ICON_GOOD;
-		} else if (restaurant.status === DATA_STATUS_WARN) {
-			row.classList.add(CSS_WARN);
-			rowHTML += ICON_WARN;
-		} else if (restaurant.status === DATA_STATUS_BAD) {
-			row.classList.add(CSS_BAD);
-			rowHTML += ICON_BAD;
-		} else if (restaurant.status === DATA_STATUS_CLOSED) {
-			row.classList.add(CSS_CLOSED);
-			rowHTML += ICON_CLOSED;
-		}
-		rowHTML += '</td>';
+		row.classList.add(STATUS_CSS[restaurant.status] || 'status-null');
+		statusCell.appendChild(makeStatusButton(restaurant.status, restaurant.statusMessage));
 		
 		// Add other fields.
-		rowHTML += '<td>' + restaurant.name + '</td>';
-		rowHTML += '<td>' + restaurant.location + '</td>';
-		rowHTML += '<td>' +
-			'<a href="' + restaurant.link + '" target="_blank">' +
+		nameCell.textContent = restaurant.name;
+		locationCell.textContent = restaurant.location;
+		linkCell.innerHTML =
+			'<a href="' + restaurant.link + '" target="_blank" role="button">' +
 				ICON_STATEMENT_LINK +
 			'</a>';
 		
-		// Add the row to the table.
-		row.innerHTML = rowHTML;
+		// Assemble the row.
+		row.appendChild(statusCell);
+		row.appendChild(nameCell);
+		row.appendChild(locationCell);
+		row.appendChild(linkCell);
+		
+		// Add the row to the list.
 		restaurant.elem = row;
 		restaurantList.appendChild(row);
 	});
